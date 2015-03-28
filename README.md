79034b7846f1f2a6bd96b88a9084e8e47cb92d93
Ernest Wong <ewong2222@hotmail.com>
2015-03-28 13:19:26 -0400

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MuseDreams
 WesternHacks project using Muse headband
+
+Pyliblo: 
+http://das.nasophon.de/pyliblo/
+1. Download Liblo first
+2. Navigate to Pyliblo directory
+3. type command "./setup.py build"
+  -troubleshooting: https://github.com/tidalcycles/Dirt/issues/18
+4. type command ".setup.py install"
+
+Liblo: 
+http://liblo.sourceforge.net/
+download liblo 0.28
+1. Navigate to Liblo directory in the terminal
+2. type command "configure"
+3. type command "make install"
+
+
--- osc_server.py
+++ osc_server.py
@@ -8,10 +8,10 @@ class MuseServer(ServerThread):
         ServerThread.__init__(self, 5001)
 
     #receive accelrometer data
-    @make_method('/muse/acc', 'fff')
-    def acc_callback(self, path, args):
-        acc_x, acc_y, acc_z = args
-        print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
+    #@make_method('/muse/acc', 'fff')
+    #def acc_callback(self, path, args):
+    #    acc_x, acc_y, acc_z = args
+    #    print "%s %f %f %f" % (path, acc_x, acc_y, acc_z)
 
     #receive EEG data
     @make_method('/muse/eeg', 'ffff')
@@ -20,14 +20,14 @@ class MuseServer(ServerThread):
         print "%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear)
 
     #handle unexpected messages
-    @make_method(None, None)
-    def fallback(self, path, args, types, src):
-        print "Unknown message \
-		\n\t Source: '%s' \
-		\n\t Address: '%s' \
-		\n\t Types: '%s ' \
-		\n\t Payload: '%s'" \
-		% (src.url, path, types, args)
+    #@make_method(None, None)
+    #def fallback(self, path, args, types, src):
+    #    print "Unknown message \
+#		\n\t Source: '%s' \
+#		\n\t Address: '%s' \
+#		\n\t Types: '%s ' \
+#		\n\t Payload: '%s'" \
+#		% (src.url, path, types, args)
 
 try:
     server = MuseServer()
