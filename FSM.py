389d1554abd282e0e3e0247fb41a75f38ef98588
Ernest Wong <ewong2222@hotmail.com>
2015-03-29 01:35:45 -0400

    more revisions--working copy

--- FSM.py
+++ FSM.py
@@ -1,7 +1,7 @@
 from threading import Timer
 import time
 
-class Main:
+class FSM():
 
     def __init__(self):
         print "starting"
@@ -9,13 +9,15 @@ class Main:
         self.timing = False
         self.state = 0
 
+    def update_blink(self, blink):
+        self.blinked = blink
+
     def state_machine(self):
         if self.blinked:
             self.state = 0
             return self.state
         else:
             if self.timing:
-                print 'return state'
                 return self.state
             else:
                 self.start_timer()
@@ -23,15 +25,8 @@ class Main:
                 return self.state
 
     def start_timer(self):
-        print "starting timer"
         self.timing = True
         t = Timer(5, self.stop).start()
 
     def stop(self):
-        print "stopped"
         self.timing = False
-
-m = Main()
-while True:
-    time.sleep(1)
-    print m.state_machine()
--- osc_server.py
+++ osc_server.py
@@ -1,6 +1,8 @@
 from liblo import *
+from threading import Timer
 import sys
 import time
+from FSM import *
 
 class MuseServer(ServerThread):
 
@@ -8,8 +10,8 @@ class MuseServer(ServerThread):
     def __init__(self):
         self.touching_forehead = False
         ServerThread.__init__(self, 5001)
+        self.fsm = FSM()
         self.blink_count = 0;
-        self.blinked = false;
 
     @make_method('/muse/elements/touching_forehead', 'i')
     def touching_forehead_callback(self, path, args):
@@ -17,10 +19,11 @@ class MuseServer(ServerThread):
 
     @make_method('/muse/elements/blink', 'i') 
     def blink_callback(self, path, args):
-        self.blinked = args[0]
-        if blinked and self.touching_forehead:
+        print "state: {}".format(self.fsm.state_machine())
+        self.fsm.update_blink(args[0])
+        if args[0] and self.touching_forehead:
             self.blink_count += 1
-            print "blinked: {}, {} times".format(blinked,self.blink_count) 
+            print "blinked:{} times".format(self.blink_count) 
 
 
 try:
@@ -30,8 +33,7 @@ except ServerError, err:
     sys.exit()
 
 server.start()
-while not server.blinked
-    #start timing
+   #start timing
 
 if __name__ == "__main__":
     while 1:
